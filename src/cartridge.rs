b1dd346ddbb27fe9c6d630e30847e77a8aebfd55
langurmonkey <me@tonisagrista.com>
2024-10-21 19:13:05 +0200

    feat: check logo in cartridge.

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use std::io::Error;
 
 /// A representation of a cartridge..
 pub struct Cartridge {
@@ -8,19 +9,31 @@ pub struct Cartridge {
     data: Vec<u8>,
 }
 
+const LOGO: [u8; 48] = [
+    0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D,
+    0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99,
+    0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E,
+];
+
 impl Cartridge {
-    pub fn new(rom: &str, check: bool) -> io::Result<Self> {
+    pub fn new(rom: &str, check_logo: bool) -> io::Result<Self> {
         let mut file = File::open(rom)?;
         let mut buffer = Vec::new();
         file.read_to_end(&mut buffer)?;
 
         // Check Nintendo logo in rom file.
-        // In 0x104 - 0x133 (260 to 307), with contents:
-        // CE ED 66 66 CC 0D 00 0B 03 73 00 83 00 0C 00 0D
-        // 00 08 11 1F 88 89 00 0E DC CC 6E E6 DD DD D9 99
-        // BB BB 67 63 6E 0E EC CC DD DC 99 9F BB B9 33 3E
-        if check {
+        // In 0x104 - 0x133 (260 to 307), with contents in LOGO.
+        if check_logo {
             //Cartridge::check_checksum(&buffer);
+            let slice = &buffer[260..307];
+            let mut i: usize = 0;
+            for u in slice.iter().cloned() {
+                if u != LOGO[i] {
+                    return Err(Error::other("Incorrect logo sequence!"));
+                }
+                i += 1;
+            }
+            println!("Correct logo sequence");
         }
 
         Ok(Self { data: buffer })
