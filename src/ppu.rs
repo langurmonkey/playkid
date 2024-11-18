b0fe438e2a531fcb47f95bece9abbe460d53bad6
langurmonkey <me@tonisagrista.com>
2024-11-18 09:12:59 +0100

    feat: new implementation of bg and win.

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,5 +1,4 @@
 use crate::constants;
-use std::collections::VecDeque;
 
 /// # PPU
 /// The PPU is the picture processing unit of our machine.
@@ -7,14 +6,12 @@ use std::collections::VecDeque;
 /// ## Video RAM
 /// The Video RAM, or VRAM, are 8 KiB located in addresses 0x8000 to 0x9FFF.
 /// A **memory bank** contains 384 tiles, or 3 tile blocks, so 6 KiB of tile data.
-/// After that, it  has two maps of 1024 bytes each (32 rows of 32 bytes each), the
-/// Background Tile Map. Each byte contains the tile number to be displayed.
+/// After that, it has two maps of 1024 bytes each (32 rows of 32 bytes each), the
+/// Background and Window tile maps. Each byte contains the tile number to be displayed.
 /// The tiles are taken from the Tile Data Table, which is at either 0x8000-0x8FFF,
-/// or 0x8800-ox97FF. In the first case, tiles are numbered as unsigned bytes (u8).
+/// or 0x8800-0x97FF. In the first case, tiles are numbered as unsigned bytes (u8).
 /// In the second case, the numbers are signed (i8), and tile 0 lies at 0x9000.
 /// The Tile Data Table address can be selected via the LCDC register.
-///
-/// In total, a bank has 8 KiB of memory.
 ///
 /// - A **tile** has 8x8 pixels, with a color depth of 2 bpp. Each tile is 16 bytes.
 ///   Tiles in a bank are typically grouped into blocks.
@@ -89,8 +86,10 @@ pub struct PPU {
     wy: u8,
     /// WX: Window X position plus 7.
     wx: u8,
-    /// Trigger for WY.
-    wy_trigger: bool,
+    /// Window line counter.
+    wly: i16,
+    /// WLY flag.
+    wly_flag: bool,
     /// BGP: Background palette register.
     bgp: u8,
     /// OBP0: Object palette 0.
@@ -111,15 +110,14 @@ pub struct PPU {
     palette: [u8; 4 * 3],
     /// Screen buffer in RGBA8888.
     pub fb: [u8; constants::DISPLAY_HEIGHT * constants::DISPLAY_WIDTH * 4],
-    /// Color buffer.
-    pub fb_col: [u8; constants::DISPLAY_HEIGHT * constants::DISPLAY_WIDTH],
+    /// Color ID buffer for priorities.
+    pub priorities: [u8; constants::DISPLAY_HEIGHT * constants::DISPLAY_WIDTH],
 }
 
 impl PPU {
     pub fn new(start_dot: u32) -> Self {
         // The 4 GB shades in RGB888.
         let palette = [224, 248, 208, 136, 192, 112, 52, 104, 86, 8, 24, 32];
-        // let palette = [24, 48, 08, 136, 192, 112, 52, 104, 86, 8, 24, 32];
 
         PPU {
             oam: [0xFF; constants::OAM_SIZE],
@@ -151,7 +149,8 @@ impl PPU {
             scx: 0,
             wy: 0,
             wx: 7,
-            wy_trigger: false,
+            wly: -1,
+            wly_flag: false,
             bgp: 0,
             obp0: 0,
             obp1: 0,
@@ -162,7 +161,7 @@ impl PPU {
 
             palette,
             fb: [0xff; constants::DISPLAY_HEIGHT * constants::DISPLAY_WIDTH * 4],
-            fb_col: [0x00; constants::DISPLAY_HEIGHT * constants::DISPLAY_WIDTH],
+            priorities: [0x01; constants::DISPLAY_HEIGHT * constants::DISPLAY_WIDTH],
         }
     }
 
@@ -170,7 +169,7 @@ impl PPU {
         self.oam.fill(0xff);
         self.vram.fill(0x00);
         self.fb.fill(0xff);
-        self.fb_col.fill(0x00);
+        self.priorities.fill(0x00);
         self.mode = 0;
         self.lcdc = 0;
         self.fdot = self.start_dot;
@@ -183,7 +182,8 @@ impl PPU {
         self.scy = 0;
         self.wx = 0;
         self.wy = 0;
-        self.wy_trigger = false;
+        self.wly = -1;
+        self.wly_flag = false;
         self.bgp = 0;
         self.obp0 = 0;
         self.obp1 = 1;
@@ -371,7 +371,7 @@ impl PPU {
 
             // V-blank.
             1 => {
-                self.wy_trigger = false;
+                self.wly_flag = false;
                 self.i_mask |= 0x01;
                 self.updated = true;
                 self.stat4
@@ -385,8 +385,9 @@ impl PPU {
 
             // Draw.
             3 => {
-                if self.lcdc5 && !self.wy_trigger && self.ly == self.wy {
-                    self.wy_trigger = true;
+                if self.lcdc5 && !self.wly_flag && self.ly == self.wy {
+                    self.wly = -1;
+                    self.wly_flag = true;
                 }
                 // No data.
                 false
@@ -403,23 +404,25 @@ impl PPU {
         self.render_sprites();
     }
 
-    /// Fetches the 8 pixels of a row within a background or window tile.
-    fn get_bgwin_tile_pixels(&mut self, tile_id: u8, line: u8, use_unsigned: bool) -> [u8; 8] {
-        let tile_data_start = self.lcdc4;
+    /// Fetches and combines the two bytes of pixel data (low/high) for a tile row,
+    /// for the background/window.
+    fn get_bgwin_tile_data(&mut self, tile_id: u8, line: u16, use_unsigned: bool) -> [u8; 8] {
+        let tile_addr_base = self.lcdc4
+            + (if use_unsigned {
+                // Unsigned.
+                tile_id as u16
+            } else {
+                // Signed.
+                (tile_id as i8 as i16 + 128) as u16
+            }) * 16;
 
-        let tile_addr = if use_unsigned {
-            tile_data_start + (tile_id as u16 * 16)
-        } else {
-            // Signed mode: convert tile_id to signed and offset within the -128 to 127 range
-            let tile_id = (tile_id as i8 as i16 + 128) as u16;
-            tile_data_start.wrapping_add(tile_id * 16)
-        };
+        let tile_addr = tile_addr_base + (line * 2);
 
-        let low_byte = self.read(tile_addr + line as u16 * 2);
-        let high_byte = self.read(tile_addr + line as u16 * 2 + 1);
+        let low_byte = self.read(tile_addr);
+        let high_byte = self.read(tile_addr + 1);
         let mut pixels = [0u8; 8];
 
-        // Each bit pair in the bytes represents a pixel color
+        // Each bit pair in the bytes represents a pixel color ID.
         for i in 0..8 {
             let color_id = ((high_byte >> (7 - i)) & 0x1) << 1 | ((low_byte >> (7 - i)) & 0x1);
             pixels[i] = color_id;
@@ -428,7 +431,7 @@ impl PPU {
         pixels
     }
 
-    /// Fetches the 8 pixels of a row within a sprite tile.
+    /// Fetches and combines the tow bytes of pixel data (low/high) of a row within a sprite tile.
     fn get_sprite_tile_pixels(
         &self,
         tile_id: u8,
@@ -468,52 +471,79 @@ impl PPU {
         pixels
     }
 
-    /// Renders a single scanline of the background and window.
+    /// Renders the background and window for the current scanline.
     fn render_bgwin_scanline(&mut self) {
-        // Determine if the background and window are enabled.
-        let bg_enabled = self.lcdc0;
-        let win_enabled = self.lcdc5 && self.ly >= self.wy;
-
         // Determine if we’re using unsigned or signed tile IDs
         let use_unsigned = (self.lcdc & 0x10) != 0;
 
+        // Is the background enabled?
+        let bg_enabled = self.lcdc0;
+
+        // Is the window enabled?
+        let wx_flag = self.wx <= 166;
+        let win_enabled = if self.lcdc5 && self.wly_flag && wx_flag {
+            self.wly += 1;
+            true
+        } else {
+            false
+        };
+
+        // We proceed only if we have the window or the background enabled.
+        if !win_enabled && !bg_enabled {
+            return;
+        }
+
+        // Every pixel in the scanline.
         for x in 0..constants::DISPLAY_WIDTH {
-            // Calculate the tile coordinates and pixel position within the tile.
-            let (tile_map, comp_x, comp_y, tile_x, tile_y) =
-                if win_enabled && (x as u8) >= self.wx.wrapping_sub(7) {
-                    let win_y = self.ly.wrapping_add(self.wy);
-                    let tile_y = win_y / 8;
-                    let win_x = x - self.wx.wrapping_sub(7) as usize;
-                    let tile_x = win_x / 8;
-                    (self.lcdc6, win_x, win_y, tile_x, tile_y)
-                } else if bg_enabled {
-                    let bg_y = self.ly.wrapping_add(self.scy);
-                    let tile_y = bg_y / 8;
-                    let bg_x = x.wrapping_add(self.scx as usize);
-                    let tile_x = bg_x / 8;
-                    (self.lcdc3, bg_x, bg_y, tile_x, tile_y)
-                } else {
-                    // Disable.
-                    self.color(x, self.ly, 0);
-                    continue;
-                };
+            let win_y = self.wly;
+            let win_x = -((self.wx as i32) - 7) + (x as i32);
 
-            // Calculate tile map address and fetch tile ID.
-            let tile_map_addr = tile_map + (tile_y as u16 * 32) + tile_x as u16;
-            let tile_id = self.read(tile_map_addr);
+            // compute tile map address, plus the pixel and the tile coordinates.
+            let (tile_map, px_x, px_y, tile_x, tile_y) = if win_y >= 0 && win_x >= 0 {
+                let win_tile_x = win_x as u16 / 8;
+                let win_tile_y = (win_y as u16 >> 3) & 31;
+                (
+                    self.lcdc6,
+                    win_x as u8 & 0x07,
+                    win_y as u16 & 0x07,
+                    win_tile_x,
+                    win_tile_y,
+                )
+            } else if bg_enabled {
+                let bg_x = self.scx as u32 + x as u32;
+                let bg_y = self.scy.wrapping_add(self.ly);
+                let bg_tile_x = (bg_x as u16 / 8) & 0x1f;
+                let bg_tile_y = (bg_y as u16 / 8) & 0x1f;
+                (
+                    self.lcdc3,
+                    bg_x as u8 & 0x07,
+                    bg_y as u16 & 0x07,
+                    bg_tile_x,
+                    bg_tile_y,
+                )
+                // let bg_x = (self.scx as u32 + x as u32) as u8 & 0x07;
+                // let bg_y = self.scy.wrapping_add(self.ly) as u16 & 0x07;
+                // let tile_x = (bg_x as u16 >> 3) & 0x1f;
+                // let tile_y = (bg_y as u16 >> 3) & 0x1f;
+                // (self.lcdc3, bg_x, bg_y, tile_x, tile_y)
+            } else {
+                // Next pixel.
+                continue;
+            };
 
-            // Get the specific row of pixels from the tile.
-            let tile_line = (comp_y % 8) as u8;
-            let tile_pixels = self.get_bgwin_tile_pixels(tile_id, tile_line, use_unsigned);
+            // Read tile ID and then data bytes.
+            let tile_id: u8 = self.read(tile_map + tile_y * 32 + tile_x);
+            let tile_pixels = self.get_bgwin_tile_data(tile_id, px_y, use_unsigned);
 
             // Get the background pixel and apply palette color.
-            let color_idx = tile_pixels[comp_x % 8];
+            let color_idx = tile_pixels[px_x as usize % 8];
             let color = (self.bgp >> (color_idx * 2)) & 0x03;
 
             // Store the pixel color in the framebuffer.
             self.color(x, self.ly, color);
         }
     }
+
     /// Fetches the sprite attributes from OAM.
     fn get_sprites_on_scanline(&self, sprite_height: u8) -> Vec<Sprite> {
         let mut sprites = Vec::new();
@@ -580,7 +610,7 @@ impl PPU {
                 // Priority handling: if bg/sprite priority bit is 0, sprite has priority over background
                 let has_priority = sprite.attributes & 0x80 == 0;
                 if has_priority
-                    || self.fb_col[self.ly as usize * constants::DISPLAY_WIDTH + x_pos] == 0
+                    || self.priorities[self.ly as usize * constants::DISPLAY_WIDTH + x_pos] == 0
                 {
                     // Store the pixel color in the framebuffer
                     self.color(x_pos, self.ly, color);
@@ -591,8 +621,8 @@ impl PPU {
 
     /// Sets the pixel at the given position to the given color id.
     fn color(&mut self, x: usize, y: u8, color_id: u8) {
-        // Color frame buffer.
-        self.fb_col[y as usize * constants::DISPLAY_WIDTH + x] = color_id;
+        // Priority frame buffer.
+        self.priorities[y as usize * constants::DISPLAY_WIDTH + x] = color_id;
         // R.
         self.fb[(y as usize * constants::DISPLAY_WIDTH + x) * 4 + 0] =
             self.palette[color_id as usize * 3];
@@ -610,7 +640,7 @@ impl PPU {
         for v in self.fb.iter_mut() {
             *v = 255;
         }
-        for v in self.fb_col.iter_mut() {
+        for v in self.priorities.iter_mut() {
             *v = 0;
         }
         self.updated = true;
@@ -655,7 +685,7 @@ impl PPU {
             self.fdot = 0;
             self.ly = 0;
             self.mode = 0;
-            self.wy_trigger = false;
+            self.wly_flag = false;
             self.clear_screen();
         }
     }
